e7ba6be4fd0f6714de4793d307086985e159ab1e
Dimitar Vatev <div007@gmail.com>
2019-02-02 14:09:02 +0200

    Conditions and tasks: 01. - 19.

--- 10. Data Aggregation - Exercise/SoftUniDB.sql
+++ 10. Data Aggregation - Exercise/SoftUniDB.sql
@@ -14,7 +14,25 @@ WHERE DepartmentID IN (2,5,7) AND HireDate > '2000-01-01'
 GROUP BY DepartmentID
 
 -- Problem 15.	Employees Average Salaries
+SELECT *
+INTO Table1
+FROM Employees
+WHERE Salary > 30000
 
+--SELECT *
+--FROM Table1
+
+DELETE
+FROM Table1
+WHERE ManagerID = 42
+
+UPDATE Table1
+SET Salary += 5000
+WHERE DepartmentID = 1
+
+SELECT DepartmentID, AVG(Salary) AS AvgSalary
+FROM Table1
+GROUP BY DepartmentID
 
 -- Problem 16.	Employees Maximum Salaries
 SELECT DepartmentID, MAX(Salary) AS MaxSalary
@@ -27,7 +45,7 @@ SELECT COUNT(Salary) AS [Count]
 FROM Employees
 WHERE ManagerID IS NULL
 
--- Problem 18.	*3rd Highest Salary -- xxx
+-- Problem 18.	*3rd Highest Salary
 SELECT DepartmentID, Salary AS [ThirdHighestSalary]
 FROM 
 (
@@ -36,9 +54,9 @@ FROM
       , DepartmentID 
 	  , Salary
     FROM Employees 
-	GROUP BY DepartmentID, Salary
 ) AS os
 WHERE DenseRank = 3
+GROUP BY DepartmentID, Salary
 
 -- Problem 19.	**Salary Challenge
 SELECT TOP(10) e.FirstName, e.LastName, e.DepartmentID --, e.Salary
@@ -50,3 +68,13 @@ FROM
 		GROUP BY DepartmentID
 	) AS a
 WHERE e.DepartmentID = a.DepartmentID AND e.Salary > a.AvgSalary
+ORDER BY DepartmentID
+
+-- 19
+SELECT TOP(10) FirstName, LastName, DepartmentID
+FROM Employees AS e
+WHERE Salary > (
+	SELECT AVG(Salary)
+	FROM Employees AS em
+	WHERE em.DepartmentID = e.DepartmentID)
+ORDER BY DepartmentID
